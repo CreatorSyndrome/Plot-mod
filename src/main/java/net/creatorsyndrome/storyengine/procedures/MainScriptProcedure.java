83c0c32cb2c140114a33850a961fb9a6259e898f
hanzo-0102 <naumov1vladimir@gmail.com>
2024-01-10 15:29:38 +0300

    Working with NPC names and delay between events

--- src/main/java/net/creatorsyndrome/storyengine/procedures/MainScriptProcedure.java
+++ src/main/java/net/creatorsyndrome/storyengine/procedures/MainScriptProcedure.java
@@ -1,5 +1,12 @@
 package net.creatorsyndrome.storyengine.procedures;
 
+import net.creatorsyndrome.storyengine.entity.NPCEntity;
+import net.creatorsyndrome.storyengine.init.StoryengineModEntities;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
@@ -15,6 +22,7 @@ import javax.annotation.Nullable;
 
 @Mod.EventBusSubscriber
 public class MainScriptProcedure {
+
 	@SubscribeEvent
 	public static void onWorldTick(TickEvent.WorldTickEvent event) {
 		if (event.phase == TickEvent.Phase.END) {
@@ -27,7 +35,18 @@ public class MainScriptProcedure {
 	}
 
 	private static void execute(@Nullable Event event, LevelAccessor world) {
+		SendMessageProcedure.execute(world, String.valueOf(world.getServer().getTickCount()), "server");
 		if (StoryengineModVariables.MapVariables.get(world).current_story_id == 0) {
+			//spawn of npc
+			if (world instanceof ServerLevel _level) {
+				Entity entityToSpawn = new NPCEntity(StoryengineModEntities.NPC.get(), _level);
+				entityToSpawn.moveTo(70, 70, 70, world.getRandom().nextFloat() * 360F, 0);
+				entityToSpawn.getPersistentData().putString("name", "randomguy");
+				if (entityToSpawn instanceof Mob _mobToSpawn)
+					_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED, null, null);
+				world.addFreshEntity(entityToSpawn);
+			}
+			SendMessageProcedure.execute(world, "Эй ты !", "Рандомный чел");
 			SetDialogVariantsProcedure.execute(world, 1, 2, 3, "Да", "Нет", "Пока", "Хочешь я подвигаюсь ?");
 		}
 		if (StoryengineModVariables.MapVariables.get(world).current_story_id == 2) {
